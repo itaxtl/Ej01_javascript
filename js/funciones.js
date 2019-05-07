1297c0bca04ab98cfbf2be77be3aa41c9c8e84a8
itaxtl <ita.xtl@gmail.com>
2019-05-07 19:51:00 +0200

    primer commit_E01

---
+++ js/funciones.js
@@ -0,0 +1,13 @@
+// 3. Esto es un comentario en una sola línea.
+/* Esto es un comentario
+ en bloque */
+
+
+
+let miNombre = prompt("Introduce nombre de usuario: ", "Ejemplo: Pedro López");
+if (miNombre == "Pepito Grillo") {
+    alert("Tenga un muy buen día Pepito Grillo!");
+}
+else {
+    alert("Buen día, pero si fueras Pepito Grillo lo tendrías mejor.");
+}
